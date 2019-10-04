8bdd31a996f849e6ba47750ca93589840a5ccbc7
U0I2747 <U0I2747 tiago.medice@gft.com>
2019-10-04 17:47:14 +0200

    ref

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,65 @@
-DELETE FROM tbl_user WHERE ID IN (1,2,3,4) ;
 
-COMMIT;
+INSERT INTO oauth_client_details (client_id,
+                                  client_secret,
+                                  scope,
+                                  authorized_grant_types,
+                                  web_server_redirect_uri,
+                                  authorities,
+                                  access_token_validity,
+                                  refresh_token_validity,
+                                  additional_information,
+                                  autoapprove)
+     VALUES ('clientId',
+             'secret',
+             'foo,read,write',
+             'password,authorization_code,refresh_token',
+             NULL,
+             NULL,
+             36000,
+             36000,
+             NULL,
+             TRUE);
 
-INSERT INTO tbl_user (id, username, password, salary, age) VALUES (1, 'jimmy', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 3456, 33);
-INSERT INTO tbl_user (id, username, password, salary, age) VALUES (2, 'jim', '$2a$04$PCIX2hYrve38M7eOcqAbCO9UqjYg7gfFNpKsinAxh99nms9e.8HwK', 7823, 23);
-INSERT INTO tbl_user (id, username, password, salary, age) VALUES (3, 'joe', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 4234, 45);
-INSERT INTO tbl_user (id, username, password, salary, age) VALUES (4, 'james', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 4234, 40);
+INSERT INTO oauth_client_details (client_id,
+                                  client_secret,
+                                  scope,
+                                  authorized_grant_types,
+                                  web_server_redirect_uri,
+                                  authorities,
+                                  access_token_validity,
+                                  refresh_token_validity,
+                                  additional_information,
+                                  autoapprove)
+     VALUES ('userId',
+             'secret',
+             'read,write,foo,bar',
+             'implicit',
+             NULL,
+             NULL,
+             36000,
+             36000,
+             NULL,
+             FALSE);
  
- COMMIT;
+              
+              
+INSERT INTO auth_details
+        VALUES (
+                  'james',
+                  '$2a$10$QBAjgmeCKjqgjMQwJARuPOlfhc33UngPh6aJbX5Ti5HVsSURc.ix2');
+              
+INSERT INTO auth_details
+        VALUES (
+                  'matthew',
+                  '$2a$10$QBAjgmeCKjqgjMQwJARuPOlfhc33UngPh6aJbX5Ti5HVsSURc.ix2');
+
+INSERT INTO auth_details
+        VALUES (
+                  'tiago',
+                  '$2a$10$WJPNuueC2XXu2F/bEet2p.g7kiTQ1pbkN3yPPGeK4p96ujmdO28UG');
+                  
+INSERT INTO auth_details
+        VALUES (
+                  'lucas',
+                  '$2a$10$WJPNuueC2XXu2F/bEet2p.g7kiTQ1pbkN3yPPGeK4p96ujmdO28UG');
+ 
